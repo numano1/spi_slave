e170f8257c1e65aec927956127e86820000ff9e8
omar <omar.numan@aalto.fi>
2025-07-09 12:58:01 +0300

    add sbt publishLocal

--- init_submodules.sh
+++ init_submodules.sh
@@ -1,41 +1,29 @@
 #!/usr/bin/env bash
-#Init submodules in this dir, if any
-DIR="$( cd "$( dirname $0 )" && pwd )"
-git submodule sync
-git submodule update --init
-
-#Publish local the ones you need
-#cd $DIR/clkdiv_n_2_4_8
-#sbt publishLocal
-
-#Selectively, init submodules of a larger projects
-#cd $DIR/rocket-chip
-#git submodule update --init chisel3
-#git submodule update --init firrtl
-#git submodule update --init hardfloat
-
-#Recursively update submodeles
-#cd $DIR/eagle_serdes
-#git submodule update --init --recursive serdes_top
-#sbt publishLocal
-#Assemble executables
-###sbt publishing
-#cd $DIR/rocket-chip/firrtl
-#sbt publishLocal
-#sbt assembly
-
-#Recursively init submodules
+#Selective initialization of submodules
+#Written by by Marko Kosunen, marko.kosunen@aalto.fi, 2017
+DIR=$( cd `dirname $0` && pwd )
 SUBMODULES="\
-    async_set_register \
+    ./async_set_register \
     "
-for module in $SUBMODULES; do
-    cd ${DIR}/${module}
-    if [ -f "./init_submodules.sh" ]; then
+
+git submodule sync
+for mod in $SUBMODULES; do 
+    git submodule update --init $mod
+    cd ${mod}
+    if [ -f ./init_submodules.sh ]; then
         ./init_submodules.sh
     fi
-    sbt +publishLocal
+    sbt publishLocal
     cd ${DIR}
+
 done
-
-
 exit 0
+
+
+
+
+
+
+
+
+
